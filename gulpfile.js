608d6651657d25fbfe65f57eb467fb6d23472cb1
Maurof18 <maurof809@gmail.com>
2024-01-22 11:16:47 -0300

    Modificaciones en package.json y otros

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-function tarea(done) {
-    console.log('mi primera tarea');
-    done();
+const {src, dest}= require('gulp');
+
+function css(done) {
+    //identificar el archivo SASS
+    //Compilarlo
+    //Almacenarla
+    
+    done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
 exports.tarea = tarea;
