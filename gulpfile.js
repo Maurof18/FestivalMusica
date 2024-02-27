ebbae0ebdc17017ac655f1711cb68dfeb0b347f4
Maurof18 <maurof809@gmail.com>
2024-02-27 11:40:49 -0300

    Creando imagenes AVIF

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const plumber= require('gulp-plumber');
 const cache= require('gulp-cache');
 const imagemin= require('gulp-imagemin');
 const webp= require('gulp-webp');
+const avif= require('gulp-avif');
 
 function css(done) {    
     src('src/scss/**/*.scss') //identificar el archivo SASS
@@ -36,7 +37,15 @@ function versionWebp(done){
 
     done();
 }
+function versionAvif(done){
+    const opciones={quality:50};
+    
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
 
+    done();
+}
 function dev(done){
     watch('src/scss/**/*.scss',css)
     done();
@@ -45,4 +54,6 @@ function dev(done){
 exports.css = css;
 exports.imagenes=imagenes;
 exports.versionWebp=versionWebp;
-exports.dev = parallel( imagenes, versionWebp, dev) ;
+exports.versionAvif=versionAvif;
+
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev) ;
