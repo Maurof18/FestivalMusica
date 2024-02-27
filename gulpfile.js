621466d091cb1b5c179d5d9eefb6cd51dce09b82
Maurof18 <maurof809@gmail.com>
2024-02-27 10:28:17 -0300

    Utilizando Gulp para Crear imagenes WEBP

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const {src, dest, watch}= require('gulp');
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber= require('gulp-plumber');
+
+//Imagenes
+const webp= require('gulp-webp');
 
 function css(done) {    
     src('src/scss/**/*.scss') //identificar el archivo SASS
@@ -11,10 +15,22 @@ function css(done) {
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function versionWebp(done){
+    const opciones={quality:50};
+    
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss',css)
     done();
 }
 
 exports.css = css;
-exports.dev=dev;
+
+exports.versionWebp=versionWebp;
+exports.dev=parallel(versionWebp,dev);
